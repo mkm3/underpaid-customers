a25f21b479cc9b4bfc2495299aa036182584f906
Michelle Macaraeg <macaraegm@gmail.com>
2020-07-10 06:59:04 +0000

    Created For Loop & Reduced # of Vars

--- accounting.py
+++ accounting.py
@@ -1,61 +1,37 @@
-melon_cost = 1.00
+def find_underpaid_customers(path):
+    """ Identify which customers underpaid """
 
-customer1_name = "Joe"
-customer1_melons = 5
-customer1_paid = 5.00
+    the_file = open(path)
+    # assigning variable to our open function, passing through produce report (path)
+    melon_cost = 1.00
 
-customer2_name = "Frank"
-customer2_melons = 6
-customer2_paid = 6.00
+    print("UNDERPAID CUSTOMER LIST:")
+    print()
 
-customer3_name = "Sally"
-customer3_melons = 3
-customer3_paid = 3.00
+    for line in the_file:
+    # iterating through each line of the produce report
 
-customer4_name = "Sean"
-customer4_melons = 9
-customer4_paid = 9.50
+        line = line.rstrip()
+        #removes the whitespace to the right of the string
 
-customer5_name = "David"
-customer5_melons = 4
-customer5_paid = 4.00
+        words = line.split('|')
+        #item is created by split string when "|" is located in line
 
-customer6_name = "Ashley"
-customer6_melons = 3
-customer6_paid = 2.00
+        customer_name = words[1]
+        customer_melons = float(words[2])
+        customer_paid = float(words[3])
+        #variables for our print statement below
 
-customer1_expected = customer1_melons * melon_cost
-if customer1_expected != customer1_paid:
-    print(f"{customer1_name} paid ${customer1_paid:.2f},",
-          f"expected ${customer1_expected:.2f}"
-          )
+        customer_expected = customer_melons * melon_cost
+        if customer_expected > customer_paid:
+          print("Customer: {}".format(customer_name))
+          print("Expected Pay: ${}".format(customer_melons))
+          print("Paid: ${}".format(customer_paid))
+          print()
 
-customer2_expected = customer2_melons * melon_cost
-if customer2_expected != customer2_paid:
-    print(f"{customer2_name} paid ${customer2_paid:.2f},",
-          f"expected ${customer2_expected:.2f}"
-          )
 
-customer3_expected = customer3_melons * melon_cost
-if customer3_expected != customer3_paid:
-    print(f"{customer3_name} paid ${customer3_paid:.2f},",
-          f"expected ${customer3_expected:.2f}"
-          )
+    the_file.close()
 
-customer4_expected = customer4_melons * melon_cost
-if customer4_expected != customer4_paid:
-    print(f"{customer4_name} paid ${customer4_paid:.2f},",
-          f"expected ${customer4_expected:.2f}"
-          )
 
-customer5_expected = customer5_melons * melon_cost
-if customer5_expected != customer5_paid:
-    print(f"{customer5_name} paid ${customer5_paid:.2f},",
-          f"expected ${customer5_expected:.2f}"
-          )
+find_underpaid_customers("customer-orders.txt")
 
-customer6_expected = customer6_melons * melon_cost
-if customer6_expected != customer6_paid:
-    print(f"{customer6_name} paid ${customer6_paid:.2f},",
-          f"expected ${customer6_expected:.2f}"
-          )
